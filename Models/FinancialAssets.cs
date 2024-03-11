ca552fd836aa9940de0fa186dcad387034369d29
Jesús Ramón <jesus.fabero@gmail.com>
2024-03-11 22:31:33 +0100

    Assets list view

---
+++ Models/FinancialAssets.cs
@@ -0,0 +1,15 @@
+﻿namespace BetsTrading_Service.Models
+{
+  public class FinancialAsset
+  {
+    public int id { get; set; }
+    public required string name { get; set; }
+    public required string group { get; set; }
+    public string? icon { get; set; }
+    public string? country { get; set; }
+  }
+
+
+}
+
+
