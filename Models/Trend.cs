56da36db0bb069d1a6e59f954abf64c73ca7a5a5
jesus.dovale <jesus.dovale@mecalux.com>
2024-06-09 18:24:50 +0200

    Added close & current fields

--- Models/Trend.cs
+++ Models/Trend.cs
@@ -3,14 +3,16 @@
   public class Trend
   {
 
-    public Trend(int id, string name, string icon, double daily_gain)
+    public Trend(int id, string name, string icon, double daily_gain, double close, double current)
 
     {
       this.id = id;
       this.name = name;
       this.icon = icon;
       this.daily_gain = daily_gain;
-     
+      this.close = close;
+      this.current = current;
+
     }
 
    
@@ -18,6 +20,8 @@
     public string name { get; private set; }
     public string icon { get; private set; }
     public double daily_gain { get; private set; }
+    public double close { get; private set; }
+    public double current{ get; private set; }
 
 
   }
